611d0106bd328591308675578caf8552bcec42ff
Ahmad Raza <razarajpoot99598@gmail.com>
2026-03-04 21:41:59 +0500

    commonJS module practiced

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+const {sum, ipAddress} = require('./Sum.js')
+require('./Test.js')
+
 let printMessage = "Let's start learning Node.js"
 
 console.log(printMessage)
+console.log(sum(2,3))
+console.log(ipAddress)
